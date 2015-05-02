aa50a8ac4d75584d2fee41c361e3c9b32f8331ef
Michael Hopkins <mhopkins321@gmail.com>
2015-05-02 17:54:28 -0600

    controller and routes

--- app/Http/Api/routes.php
+++ app/Http/Api/routes.php
@@ -4,4 +4,5 @@ Route::get('/', 'ApiController@index');
 
 Route::group(['prefix' => 'cb'],function(){
     Route::get('/','CBController@index');
+    Route::get('organization/{uuid}','CBController@show');
 });
