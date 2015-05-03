b31f3b5344ab43d4027e2d8af21f1282017f5c21
Michael Hopkins <mhopkins321@gmail.com>
2015-05-02 19:48:13 -0600

    add faker parameters

--- app/Entities/Organization.php
+++ app/Entities/Organization.php
@@ -1,10 +1,19 @@
 <?php namespace CS\Entities;
 
+use Faker\Factory;
+
 class Organization extends BaseEntity
 {
     public function __construct()
     {
-        parent::__construct('organization');
+        $f = Factory::create();
+        parent::__construct('organization',[
+            'permalink' => $f->text(30),
+            'api_path' => $f->url,
+            'web_path' => $f->url,
+            'name' => $f->company,
+            'also_known_as' => [$f->company,$f->company]
+        ]);
     }
     public function find($uuid)
     {
