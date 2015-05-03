bc7b8f16e2dd98aad7ef1debdf27db3e2ca8edef
Michael Hopkins <mhopkins321@gmail.com>
2015-05-03 06:22:39 -0600

    pruning

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -5,11 +5,9 @@ Route::get('test','TestController@get');
 Route::post('test','TestController@post');
 
 Route::group(['prefix' => 'api'],function() {
-    Route::get('log','SentimentController@fromSlack');
-    Route::post('log',function(){
-        Queue::marshal();
-    });
-    Route::post('organization', 'SentimentController@test');
+    Route::post('organization', 'OrganizationsController@findOrSearch');
     Route::post('person','PersonsController@findOrSearch');
+
+
     #Route::resource('people', 'PersonsController');
 });
